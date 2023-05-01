0f3e3b9adfbdbdabf4f4604fea6d9a5d8f0b729d
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-05-01 21:40:56 +0600

    create chef info

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+
+const chefData = require('./chef.json')
+
 const port = 5000
 
 app.get('/', (req , res)=>{
-    res.send('hello world this is my server')
+    res.send(chefData)
 })
 
 app.listen(port, () => {
