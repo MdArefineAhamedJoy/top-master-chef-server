40cce168c7bc93785739b09f4e86bbbf5ae655f9
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-05-01 23:07:49 +0600

    instal cors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 
+app.use(cors())
 const chefData = require('./chef.json')
 
 const port = 5000
 
-app.get('/', (req , res)=>{
+app.get("/", (req , res)=>{
+    res.send("hello")
+})
+app.get('/topChef', (req , res)=>{
     res.send(chefData)
 })
 
